4684e10550f144a9e7107fa7097b3a430b5631f4
JAMAL-111 <jamalkasule123@gmail.com>
2025-11-04 21:12:17 -0500

    Created Auth Controller And Tested It Successfully

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    //Registers
+    public function register(Request $request){
+        $validated = $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'phone' => 'required|string|unique:users,phone',
+            'password' => [
+                'required',
+                'confirmed',
+                Password::min(8)->mixedCase()->numbers()->symbols(),
+            ]
+        ]);
+
+        $user = User::create([
+            'first_name' => $validated['first_name'],
+            'last_name' => $validated['last_name'],
+            'email' => $validated['email'],
+            'phone' => $validated['phone'],
+            'password' => Hash::make($validated['password']),
+            'role' => 'buyer',
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    //Login with either the phone number or email
+    public function login(Request $request){
+        $request->validate([
+            'identifier' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        //Identifying whether it is an email or a phone number
+        $user = User::where('email', $request->identifier)
+                    ->orWhere('phone', $request->identifier)
+                    ->first();
+
+        if(! $user || !Hash::check($request->password, $user->password)){
+            return response()->json(['message' => 'Invalid Credentials.'], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login Successful',
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    //Logout a user and invalidate the current token
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out succcessfully.']);
+    }
+
+    //Retrieving the profile of a logged in user
+    public function profile(Request $request){
+        return response()->json($request->user());
+    }
+}
